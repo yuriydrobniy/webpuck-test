c81702e9607f62e92c567faed943fa5a3279abaa
Yuriy Drobniy <yuriy.drobniy1@gmail.com>
2016-05-23 22:25:30 +0300

    third

--- build.js
+++ build.js
@@ -1,3 +1,18 @@
 /**
  * Created by yura on 22.05.2016.
  */
+var path = require('path');
+console.log('path ' + path);
+var webpack = require('webpack');
+console.log('webpack ' + webpack);
+var config = {
+    context: path.join(__dirname, './'), // исходная директория
+    entry: './index', // файл для сборки, если несколько - указываем hash (entry name => filename)
+    output: {
+        path: path.join(__dirname, 'assets') // выходная директория
+    }
+};
+var compiler = webpack(config);
+compiler.run(function (err, stats) {
+    console.log(stats.toJson()); // по завершению, выводим всю статистику
+});
